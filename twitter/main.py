197554d4ee6214d5bc755e5818f39dc1c31c11b7
Mike Spadaru <maspadaru@tutanota.com>
2020-07-08 12:31:33 +0200

    raw_trident - query trident from python using bash commmand

--- twitter/main.py
+++ twitter/main.py
@@ -3,6 +3,15 @@ import time
 
 from twitter import TwitterApi
 from triple_extractor import TripleExtractor 
+import raw_trident
+
+def link_entities(triple):
+    fsubj, subj = raw_trident.query(triple['subject'])
+    frel, rel = raw_trident.query(triple['relation'])
+    fobj, obj = raw_trident.query(triple['object'])
+    if fsubj and frel and fobj:
+        return (subj, rel, obj)
+    return None
 
 
 def handle_tweet(tweet, trpext):
@@ -17,12 +26,17 @@ def handle_tweet(tweet, trpext):
         triples = trpext.get_triples(clean_tweet)
         for triple in triples:
             # print('|-', triple)
-            ares_triple_frmt = "{1} {0} {2}".format(
-                    triple['subject'], triple['relation'], triple['object'])
+            # ares_triple_frmt = "{1} {0} {2}".format(
+                    # triple['subject'], triple['relation'], triple['object'])
             readable_triple_frmt = "{0} :: {1} :: {2}".format(
                     triple['subject'], triple['relation'], triple['object'])
             # print(ares_triple_frmt)
             print(readable_triple_frmt)
+            rez = link_entities(triple)
+            if rez != None:
+                print("Found entities!")
+            else:
+                print ("Entities not found!")
 
 def get_twitter_input(trpext):
     filter=["Amsterdam", "Stockholm", "Copenhagen", "Toronto"]
@@ -30,10 +44,10 @@ def get_twitter_input(trpext):
     while True: 
         # print(" ")
         # print("=== ============== START TIMEPOINT ==================== ==== ")
-        print(" ")
         tweets = twitter_api.get_tweets()
         for tweet in tweets:
             handle_tweet(tweet, trpext)
+        print(" ") # printing empty line after each time-point
         time.sleep(1)
 
 def get_file_input(file_path, trpext):
---
+++ twitter/raw_trident.py
@@ -0,0 +1,45 @@
+import os
+import subprocess
+
+TRIDENT_EMPTY_RESULT = "<empty result>"
+TRIDENT_KNOWN_EMPTY = "<empty result -- known empty>"
+TRIDENT_PATH="../../trident/"
+TRIDENT_EXEC="build/trident"
+DATASTORE="data/wdumpwebsite"
+QUERY_FILE="temp.qry"
+BASH_COMMAND = "{0}{1} query -i {0}{2} -q {3}".format(TRIDENT_PATH, 
+        TRIDENT_EXEC, DATASTORE, QUERY_FILE) 
+QUERY_STRING = """
+SELECT ?s WHERE {{
+  ?s <http://www.w3.org/2000/01/rdf-schema#label> "\\\"{0}\\\"@en "
+}}
+LIMIT 1
+    """ 
+
+def query(entity):
+    """
+    (str) -> (bool, str) 
+     - based on params generate query string
+     - write query to file
+     - query trident and store result
+     - close and remove query file
+     - parse result 
+    """
+    query_string = QUERY_STRING.format(entity)
+    query_file = open(QUERY_FILE, "w+")
+    query_file.write(query_string)
+    query_file.close()
+    print(BASH_COMMAND)
+    process = subprocess.Popen(BASH_COMMAND.split(), stdout=subprocess.PIPE, 
+            stderr=subprocess.DEVNULL)
+    output, error = process.communicate()
+    output = output.decode("utf-8").strip()
+    print("output = $", output , "$")
+    # os.remove(QUERY_FILE)
+    if (len(output) > 0 and output != TRIDENT_EMPTY_RESULT
+            and output != TRIDENT_KNOWN_EMPTY):
+        # print ("TRUE")
+        return (True, output)
+    # print ("FALSE")
+    # print ("RESULT NOT FOUND!")
+    return (False, "")
